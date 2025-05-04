35dd91cbd9d24482f78224b6da8765e231d3311c
ShivendraSinha418 <shivendrasinha60@gmail.com>
2025-05-04 13:21:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,4 +71,5 @@ def predict():
     return render_template('predict.html', prediction=prediction, confidence=confidence, video_path=video_path)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
